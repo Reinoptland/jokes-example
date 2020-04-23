73c18b64ee9943fd96db4beb448dc189a1ab4f7b
Reinoptland <reinoptland@gmail.com>
2020-04-23 20:00:38 +0200

    Define jokes object

---
+++ index.js
@@ -0,0 +1,24 @@
+const jokes = {
+  sql: {
+    setup: "A SQL query walks into a bar, walks up to two tables and asks...",
+    punchline: "Can I join you?",
+  },
+  java: {
+    setup: "Why do Java programmers wear glasses?",
+    punchline: "Because they don't C#",
+  },
+  ui: {
+    setup: "A user interface is like a joke.",
+    punchline: "If you have to explain it then it is not that good.",
+  },
+  ifelse: {
+    setup:
+      "A programmer puts two glasses on his bedside table before going to sleep.",
+    punchline:
+      "A full one, in case he gets thirsty, and an empty one, in case he doesn’t.",
+  },
+  ie: {
+    setup: "How do you check if a webpage is HTML5?",
+    punchline: "Try it out on Internet Explorer",
+  },
+};
