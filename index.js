7979e5674d5dd359b1a89aae4370aa4120dcf672
Reinoptland <reinoptland@gmail.com>
2020-04-23 21:55:26 +0200

    Add env variable for PORT

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ const express = require("express");
 // create server
 const app = express();
 
-// 3000 is common
-const port = 3000;
+// process.env.PORT when deploying the app, 3000 in development
+const port = process.env.PORT || 3000;
 
 // confirmation function
 function onListen() {
